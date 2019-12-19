5364c4cdacd6671d867ef5912d18d54716fd07a6
Alexander Persiyanov <persiyanov1990@hotmail.com>
2019-12-19 08:26:45 +0100

    creato ListComponent

--- src/Components/ListComponent/ListComponent.jsx
+++ src/Components/ListComponent/ListComponent.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React ,{useRef} from "react";
+import "./ListComponent.scss";
 import { connect } from "react-redux";
 
 
 const mapStateToProps = (state) => {
-  return { new_articles: state.articles };
+  return { list: state.list };
+};
+const mapStateToDispatch = (dispatch) => {
+
+  return { 
+    onAddListItem: (data) => { dispatch ({type:"ADD_LIST_ITEM",data }) } ,
+  };
 };
 
-function ConnectedList ({new_articles}) { 
+function ConnectedList (props) { 
 //Distructorization obj
 //let {new_articles} = props;
+
+const inputRef = useRef(null);
+
+function actionButton(){
+  props.onAddListItem(inputRef.current.value);
+  inputRef.current.value = "";
+ 
+}
    
     return (
-  
+      <div className="list-container">
+      <input type="text" ref={inputRef} /> 
+      <button onClick={actionButton}>Add</button>
       <ul>
-        {new_articles.map(el => (
-          <li key={el.id}>{el.title}</li>
+        {props.list.map(el => (
+        <li key={el.id}>id:{el.id}__{el.title}</li>
         ))}
       </ul>
+      </div>
     );
 }
 
-export default connect(mapStateToProps)(ConnectedList);
+export default connect(mapStateToProps,mapStateToDispatch)(ConnectedList);
